e47d1447c1ec53c1bfe6d14f8cd297f30a8953cf
Iqbal Fasri <iqbalx64@gmail.com>
2019-10-25 15:11:27 +0700

    add: address order

--- src/screens/AddressOrder.screen.js
+++ src/screens/AddressOrder.screen.js
@@ -1,22 +1,50 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 // Own component
 import {Layout} from '../components/HiasLayout';
 import TopBar from '../components/HiasTopBar';
 import {Actions} from 'react-native-router-flux';
 
+const temproraryData = [
+  {
+    id: 1,
+    first_name: 'John',
+    last_name: 'Black',
+    company_name: 'Hias House',
+    town_city: 'DKI Jakarta',
+    post_code: '1234',
+    address:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting indu',
+    email: 'johnblack@gmail.com',
+    phone_number: '08960542423',
+  },
+];
+
 function AddressOrder(props) {
-  console.log(props, "Address prop")
+  const [addressDetail, setAddressDetail] = useState(temproraryData);
+
+  useEffect(() => {
+    console.log(addressDetail, 'Detail data');
+  }, []);
 
   return (
     <Layout>
-      <Text>AddressOrder</Text>
-      <TouchableOpacity onPress={() => Actions.jump('BillingDetail', { test: "halo" })}>
+      <View style={styles.addressRow}>
+        
+      </View>
+      <TouchableOpacity
+        onPress={() => Actions.jump('BillingDetail', {test: 'halo'})}>
         <Text>Next</Text>
       </TouchableOpacity>
     </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  addressRow: {
+    flexDirection: 'row',
+  },
+});
 
 export default AddressOrder;
