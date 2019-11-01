290e477209a64fe0922336168467df4eaf1e8a73
Iqbal Fasri <iqbalx64@gmail.com>
2019-11-01 15:39:14 +0700

    refactor: from lib.js to utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,12 +1,5 @@
 import {Dimensions, Platform} from 'react-native';
 
-// rupiah formatter
-export const toRupiah = price => {
-  return Math.abs(price)
-    .toString()
-    .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');
-};
-
 // get device width
 export const getDeviceWidth = Dimensions.get('window').width;
 
@@ -15,6 +8,13 @@ export const getDeviceHeight = Dimensions.get('window').height;
 
 // cek device is android
 export const isAndroid = Platform.OS === 'android';
+
+// rupiah formatter
+export const toRupiah = price => {
+  return Math.abs(price)
+    .toString()
+    .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');
+};
 
 // shorten long text
 export const getShortString = (longString, maxLength) => {
